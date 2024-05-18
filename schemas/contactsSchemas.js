5d1bd3dcc6e60bf6833a651cb084826ea0e3f6ef
maxmelnyk1311 <141562499+maxmelnyk1311@users.noreply.github.com>
2024-05-18 16:37:16 +0300

    Update contactsSchemas.js

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -7,9 +7,9 @@ export const createContactSchema = Joi.object({
 })
 
 export const updateContactSchema = Joi.object({
-    name: Joi.string().required().min(3).max(30),
-    email: Joi.string().required().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
-    phone: Joi.string().required().min(7).max(12),
+    name: Joi.string().optional().min(3).max(30),
+    email: Joi.string().optional().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+    phone: Joi.string().optional().min(7).max(12),
 })
 
 export const updateFavoriteInContact = Joi.object({
